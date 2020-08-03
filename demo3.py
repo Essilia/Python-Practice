b9e57baa69673c46edf2e7140e562b61363202c4
Essilia <1280550032@qq.com>
2020-08-03 18:42:07 +0800

    python的方法定义

---
+++ demo3.py
@@ -0,0 +1,24 @@
+# import time
+# import random
+
+
+# a=random.randint(1,4)
+# print(a)
+
+
+# 定义方法
+#def 方法名(参数1，参数2)：   //参数可加可不加
+#逻辑代码块
+def jiafa(a,b):
+    print(a+b) 
+a=jiafa(1,2)
+print(1,"+",a,"=")
+
+
+#定义类
+#class 类名():  //类名的首字母要大写
+
+
+# 面向对象编程
+
+   
