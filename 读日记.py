bb964b509ca10d238ead9d4aa132cc0ef5366c89
Essilia <1280550032@qq.com>
2020-08-05 20:48:38 +0800

    读日记

--- 读日记.py
+++ 读日记.py
@@ -1,5 +1,6 @@
 
 with open("F:\日记.txt","r",encoding="utf-8") as f:
-    f.readline(now)
-    f.readline(text)
-    f.read("......................."+"\n")
+    
+    text=f.readlines()
+for i in text:
+    print(i)
