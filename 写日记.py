ce51b3bfe0df1615661fe41faf27e5b31675a556
Essilia <1280550032@qq.com>
2020-08-07 11:35:10 +0800

    20.8.6

--- 写日记.py
+++ 写日记.py
@@ -1,5 +1,5 @@
-import time
-now=time.strftime("%y-%m-%d %H:%M:%S")
+import time     #引入第三方time包
+now=time.strftime("%y-%m-%d %H:%M:%S")     #获取当前系统时间：年月日用小写，时分秒用大写
 text=input("请输入今天的心情：")
 with open("F:\日记.txt","a",encoding="utf-8") as f:
     f.write(now+"\n")
